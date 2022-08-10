9ffd6ffc1bc4fc884fae296ffcba084474704380
komesshhh <komeshsiva@gmail.com>
2022-08-10 19:47:00 +0530

    Check how many characters present in a string and print

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
-a=input()
-b=""
-for i in range(len(a)):
-    ch=int(ord(a[i]))+1       # convert charater to ascii value.
-    b=b+chr(ch)                # chr() - is used to convert int to character.
-print(b.replace("!"," "))
+string="Sample test case"
+duplicates=[]
+for i in string:
+  if string.count(i)>1:
+    if i not in duplicates:
+      duplicates.append(i)
+print(*duplicates)
